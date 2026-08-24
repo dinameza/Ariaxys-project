87663cc694ae242213f4ec6815c0897288545c76
dinameza <dinamezah26@gmail.com>
2026-08-24 12:19:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const nav = document.getElementById('navLinks');
+    nav.classList.toggle('active');
+}
+
+// Cerrar menú al hacer clic en un enlace (para móviles)
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('navLinks').classList.remove('active');
+    });
+});
