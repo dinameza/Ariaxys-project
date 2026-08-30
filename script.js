a21752b3766aae97485d36e56b60a399d3294b9d
dinameza <dinamezah26@gmail.com>
2026-08-30 15:45:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ function toggleMenu() {
     nav.classList.toggle('active');
 }
 
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('navLinks').classList.remove('active');
+    });
+});function toggleMenu() {
+    const nav = document.getElementById('navLinks');
+    nav.classList.toggle('active');
+}
+
 // Cerrar menú al hacer clic en un enlace (para móviles)
 document.querySelectorAll('.nav-links a').forEach(link => {
     link.addEventListener('click', () => {
